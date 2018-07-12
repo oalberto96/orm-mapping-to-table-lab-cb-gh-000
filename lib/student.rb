d1ee46707a7073f19cd0f04d4a3bc94dcabfe416
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-12 23:21:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,9 @@
 class Student
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(name, grade)
     @name = name
     @grade = grade
